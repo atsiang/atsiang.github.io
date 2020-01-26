1c2b83f3bf32e3e1a4e0e9fbf6b0d9827ec8c1f0
atsiang <56285060+atsiang@users.noreply.github.com>
2020-01-25 22:14:37 -0500

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+if($_POST['psubmit']) {
+	$firstname = $_POST['pfirstname'];
+	$lastname = $_POST['plastname'];
+	$name = $firstname . ' ' . $lastname;
+	$email = $_POST['pemail'];
+	$message = $_POST['pmessage'];
+	$from = 'From: Portfolio Site Contact Form';
+	$to = 'amandajtsiang@gmail.com';
+	$subject = 'New message for Manda';
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+	
+	if(mail ($to, $subject, $body, $from)){
+		echo '<script type="text/javascript">alert("Thanks, your message has been sent!"); location="/contact.html"</script>';
+	}
+	
+	else{
+		echo '<script type="text/javascript">alert("Oops...There was a problem sending your message."); location="/contact.html"</script>';
+	}
+}
+?>
